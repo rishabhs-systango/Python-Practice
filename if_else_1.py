7e0c1d655b807de1511c17392168030f9f243299
rishabhs-systango <rbsharma@isystango.com>
2021-06-18 17:44:39 +0530

    if/else : checking voting eligibilty

---
+++ if_else_1.py
@@ -0,0 +1,6 @@
+# program to check eligibilty for voting
+age = int(input('Enter your age\n'))
+if age >= 18 : 
+    print('Your are eligible for voting')
+else : 
+    print('Your are not eligible for voting')
