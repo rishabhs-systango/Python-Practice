d87a9ca72af0e21eefbec3078e94d47cb54219df
rishabhs-systango <rbsharma@isystango.com>
2021-06-18 18:41:17 +0530

    for_loop : simple program

---
+++ for_loop.py
@@ -0,0 +1,5 @@
+# fetching cars present in a garage one by one
+garage = ['Thar','Creta','Kia Seltos','Scorpio','Fortuner','Bolero']
+print("cars present in garage")
+for car in garage : 
+    print(car)
