d5635539c1d94e14795ff72b2fb4e44f76968197
rishabhs-systango <rbsharma@isystango.com>
2021-06-18 17:52:58 +0530

    if/else : converting percentage to grade

---
+++ if_else_2.py
@@ -0,0 +1,16 @@
+# calculating grade through percentage and pass or fail
+percentage = float(input('Enter your percentage\n'))
+if percentage >= 90 :
+    print('you are pass and your grade is A+ \n')
+elif percentage >=80 :
+     print('you are pass and your grade is A \n')
+elif percentage >=70 :
+     print('you are pass and your grade is B+\n')
+elif percentage >=60 :
+     print('you are pass and your grade is B \n')
+elif percentage >=50 :
+     print('you are pass and your grade is C\n')
+elif percentage >=40 :
+     print('you are pass and your grade is D \n')
+elif percentage <40 :
+     print('you are Fail and your grade is F \n')
