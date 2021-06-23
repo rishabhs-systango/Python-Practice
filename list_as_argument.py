0a86631516b95e6748beb43e424dbe174a60dc3b
rishabhs-systango <rbsharma@isystango.com>
2021-06-23 20:05:50 +0530

    Function : List as argument

---
+++ list_as_argument.py
@@ -0,0 +1,6 @@
+# passing list of studentName as a argument
+def studentNames(list) : 
+    for item in list :
+        print(item)
+listOfStudentName = ["Rishabh","Somesh","Sid","Raj","Karan"]
+studentNames(listOfStudentName)
