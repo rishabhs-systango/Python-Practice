2ea2409ee0dbc8778840f17dbaba89969300ea09
rishabhs-systango <rbsharma@isystango.com>
2021-06-23 23:09:44 +0530

    Number : Simple Practice Program

---
+++ Number.py
@@ -0,0 +1,21 @@
+import math
+# simple program practicing methods of Number 
+a = 120
+b = 20.5
+#checking type of a and b
+print(type(a))
+print(type(b))
+
+# type conversion
+c = int(b)
+print(c)
+
+# some functions
+print(abs(-40))
+print(math.ceil(b))
+print(math.floor(b))
+print(math.log(a))
+print(math.log10(a))
+print(max(2,4,10,20,4.5))
+print(min(2,4,10,20,4.5))
+print(math.sqrt(b))
