c5e6199da30f5007cf3841550ce494fcbe69e62e
rishabhs-systango <rbsharma@isystango.com>
2021-06-24 15:11:09 +0530

    Tuple : practice all methods

---
+++ tuple.py
@@ -0,0 +1,16 @@
+tuple = (1,'hy',2,4,5)
+print(tuple[1])
+print(tuple[0:3])
+
+#conactenation and repetion
+tuple2 = tuple + tuple * 2
+print(tuple2)
+
+#some methods
+tuple3 = (1,2,5,79,6)
+print(len(tuple2))
+print(max(tuple3))
+print(min(tuple3))
+ 
+#deleting tuple
+del tuple3
